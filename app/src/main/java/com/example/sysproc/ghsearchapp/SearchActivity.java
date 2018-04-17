bfff3e1f3981aefe8ff009ec6fefd27dbb882846
SysProc <sysproc666@gmail.com>
2018-04-18 09:51:22 +1000

    Pony.png renamed to unicorn.png. Added new values in sting.xml. Added SearchView and overflow menu in SearchActivity's toolbar.

--- app/src/main/java/com/example/sysproc/ghsearchapp/SearchActivity.java
+++ app/src/main/java/com/example/sysproc/ghsearchapp/SearchActivity.java
@@ -1,7 +1,12 @@
 package com.example.sysproc.ghsearchapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -12,4 +17,29 @@ public class SearchActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_search; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
 }
